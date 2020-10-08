4ae8c8185d455eb00c7760695c662423081a37e3
Скоробогатый Евгений <evskorobogatij@gmail.com>
2020-10-08 11:04:56 +0300

    Зубная карта со состояниями зубов

--- src/components/Tooth/Tooth.tsx
+++ src/components/Tooth/Tooth.tsx
@@ -5,6 +5,8 @@ import {ReactComponent as Incisors} from './../../img/incisors-tooth.svg';
 import {ReactComponent as Molar} from './../../img/molar-tooth.svg';
 import {ReactComponent as Premolar} from './../../img/premolar.svg';
 import {ReactComponent as ToothImg} from './../../img/tooth.svg';
+import {ReactComponent as FakeTooth  } from "./../../img/fake-tooth.svg";
+import {ReactComponent as Implant } from "./../../img/implant-fixture.svg";
 
 import "./Tooth.scss";
 
@@ -16,10 +18,20 @@ export enum ToothType {
     Tooth
 }
 
+export enum ToothStatus {
+    Normal,
+    Caries,
+    CariesPlomb,
+    Implant,
+    Fake,
+    Removed
+}
+
 export interface IToothProps {
     num : number, //номер зуба
     type ?: ToothType,
     active ?: boolean,
+    status : ToothStatus,
     onClick?: (param : React.MouseEvent<HTMLDivElement, MouseEvent> ) => void
 }
 
@@ -29,22 +41,36 @@ export default function Tooth(props: IToothProps) {
       rotate : '180deg'
   } : {}
 
-  const toothType=(num:number)=>{
+  const checkToothHelth=(status:ToothStatus) =>{
+      let s = '';
+      s+=status===ToothStatus.Caries ? 'Tooth_caries' : '';
+      s+=status===ToothStatus.CariesPlomb ? 'Tooth_caries_plomb' : ''
+      return s;
+  }
+
+  const toothType=(props:IToothProps)=>{
+        const {num,status} = props
         const q = Math.floor(num/10)
         const pos = num % 10
 
         console.log(q,pos)
+        if(status===ToothStatus.Removed)
+            return <></>
+        if(status===ToothStatus.Fake)
+            return <FakeTooth style={rotated_tooth} />
+        if(status===ToothStatus.Implant)
+            return <Implant style={rotated_tooth}  />
         if([1,2].includes(pos))
-            return <Incisors style={rotated_tooth}  />
+            return <Incisors style={rotated_tooth} className={checkToothHelth(status)} />
         if(pos===3)
-            return <Canine style={rotated_tooth} />
+            return <Canine style={rotated_tooth} className={checkToothHelth(status) } />
         if([4,5].includes(pos)) 
-            return <Premolar style={rotated_tooth}  />
+            return <Premolar style={rotated_tooth} className={checkToothHelth(status)} />
         if([6,7,8].includes(pos)){
             if([1,2].includes(q))
-                return <Molar style={rotated_tooth} />
+                return <Molar style={rotated_tooth} className={checkToothHelth(status)} />
             if([3,4].includes(q))    
-                return <ToothImg style={rotated_tooth} />
+                return <ToothImg style={rotated_tooth} className={checkToothHelth(status)} />
         }
   }
 
@@ -53,7 +79,7 @@ export default function Tooth(props: IToothProps) {
         <span>{props.num}</span>
         {/* <ToothImg style={rotated_tooth} /> */}
         <div>
-           { toothType(props.num)}
+           { toothType(props)}
         </div>
     </div>
   );
