0e9f1532c88e48ca220784c02fb24ea461a51eb8
Скоробогатый Евгений <evskorobogatij@gmail.com>
2020-10-12 15:40:19 +0300

    fix

--- src/components/ToothInfo/ToothInfo.tsx
+++ src/components/ToothInfo/ToothInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import  React, { useContext } from 'react';
 import ContextTooth from '../../context';
 import { ToothStatus } from '../Tooth/Tooth';
 
@@ -6,44 +6,39 @@ import "./ToothInfo.scss"
 
 function ToothInfo() {
 
-    const { currentTooth, tooths, toothStatusChange } = useContext(ContextTooth);
+    const {currentTooth, tooths, toothStatusChange} = useContext(ContextTooth);
 
-
-    interface IToothStatus {
-        value: ToothStatus,
+    const toothStatuses:Array<{
+        value : ToothStatus,
         title: string
-    }
+    }> =[
+        {value:ToothStatus.Normal, title:'Здоровый зуб'},
+        {value:ToothStatus.Caries, title:'Кариес'},
+        {value:ToothStatus.CariesPlomb, title:'Пломба'},
+        {value:ToothStatus.Implant, title:'Имплант'},
+        {value:ToothStatus.Fake, title:'Искуственный зуб'},
+        {value:ToothStatus.Removed, title:'Удален'}
+    ]   ;
 
-    const toothStatuses: Array<IToothStatus> = [
-        { value: ToothStatus.Normal, title: 'Здоровый зуб' },
-        { value: ToothStatus.Caries, title: 'Кариес' },
-        { value: ToothStatus.CariesPlomb, title: 'Пломба' },
-        { value: ToothStatus.Implant, title: 'Имплант' },
-        { value: ToothStatus.Fake, title: 'Искуственный зуб' },
-        { value: ToothStatus.Removed, title: 'Удален' }
-    ];
-
-    const statuses = (toothStatuses: Array<IToothStatus>) => {
-        toothStatuses.map((item, index) => (
-
-            <label key={index} className="radio">
-                <input type="radio" value={item.value} checked={currentTooth >= 0 ? tooths[currentTooth].status === item.value : false} onChange={toothStatusChange?.bind(null, currentTooth, item.value)} />
-                <div className="radio__text">{item.title}</div>
-            </label>
-        ))
-
-    }
-
-
-    return (
+    return (        
         <div className={'ToothInfo'}>
-            <p>{currentTooth >= 0 ? `Зуб № ${tooths[currentTooth].num}` : 'Зуб не выбран'} </p>
             {
-                currentTooth >= 0 ? statuses(toothStatuses) : ''
-
+                currentTooth ? '' : ''                 
             }
-        </div>
-    )
+            <p>{ currentTooth>=0 ? `Зуб № ${tooths[currentTooth].num}` : 'Зуб не выбран' } </p>    
+            {
+                currentTooth>=0 ?
+                toothStatuses.map((item,index)=>(
+
+                    <label key={index} className="radio">
+                        <input type="radio" value={item.value} checked={ currentTooth>=0 ? tooths[currentTooth].status===item.value : false} onChange={toothStatusChange?.bind(null,currentTooth,item.value)}  /> 
+                        <div className="radio__text">{item.title}</div>
+                    </label>
+                ))
+                : ''
+            }       
+        </div>    
+    )    
 }
 
 export default ToothInfo;
