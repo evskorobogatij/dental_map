57b4d50a4b83f1fdf1ac4afaa0c81747ccedc8f6
Скоробогатый Евгений <evskorobogatij@gmail.com>
2020-10-12 14:43:31 +0300

    Updates

---
+++ precache-manifest.e390af7ed32716a7effe71bd6d0ebd96.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0a38d06e1328213564c7d2da72fa2b",
+    "url": "/dental_map/index.html"
+  },
+  {
+    "revision": "2644acf6a98138cb7eeb",
+    "url": "/dental_map/static/css/main.9c61b06e.chunk.css"
+  },
+  {
+    "revision": "7e3003f5d0b1cdba1a4c",
+    "url": "/dental_map/static/js/2.20187bea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dental_map/static/js/2.20187bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2644acf6a98138cb7eeb",
+    "url": "/dental_map/static/js/main.12c7ff94.chunk.js"
+  },
+  {
+    "revision": "b7b3d0e1a8a26feca941",
+    "url": "/dental_map/static/js/runtime-main.54857105.js"
+  },
+  {
+    "revision": "134730e3c7550b18cb396095f1d4dd40",
+    "url": "/dental_map/static/media/canine-tooth.134730e3.svg"
+  },
+  {
+    "revision": "6bfb5d63f8bff46dc5df855b1769f714",
+    "url": "/dental_map/static/media/fake-tooth.6bfb5d63.svg"
+  },
+  {
+    "revision": "5fda534a03b591410b79e767e0690ec9",
+    "url": "/dental_map/static/media/implant-fixture.5fda534a.svg"
+  },
+  {
+    "revision": "dfad8635768f64c4cd2f9a2b079eb71c",
+    "url": "/dental_map/static/media/incisors-tooth.dfad8635.svg"
+  },
+  {
+    "revision": "91da3b993ab3abfe9ca7a9681fc146f9",
+    "url": "/dental_map/static/media/molar-tooth.91da3b99.svg"
+  },
+  {
+    "revision": "30cc4deb586b84ab2762d7f13734ac27",
+    "url": "/dental_map/static/media/premolar.30cc4deb.svg"
+  },
+  {
+    "revision": "dce879b8ccda388affa2665fbed0f50b",
+    "url": "/dental_map/static/media/tooth.dce879b8.svg"
+  }
+]);
